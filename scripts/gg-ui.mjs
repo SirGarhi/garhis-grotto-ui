f50af0f96d2c52020426b32f20d5c90072e4a14b
garhi <hrf.services+github@proton.me>
2024-12-30 17:19:03 -0800

    cleanup

---
+++ scripts/gg-ui.mjs
@@ -0,0 +1,9 @@
+
+Hooks.once("ready", () => {
+	console.warn("GG | Initializing Garhi's Grotto - UI");
+	$('body.vtt').addClass('garhis-grotto-ui');
+})
+
+// Hooks.on('setup', () => {
+//     registerSettings();
+// });
